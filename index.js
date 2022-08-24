810e5c3bb055a13249e575a2f5274efe78fdbfb7
Bryan Meadows <bryan1993meadows@gmail.com>
2022-08-24 14:00:55 -0600

    commit tree c5e7233c6a589a240a5ffb4c393ab7bda40a02fd

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, name) {
+  return array.filter(function(element) {
+    return element.toLowerCase().startsWith(name.toLowerCase());
+  }).sort();
+}
+// does not return drivers if only middle or ending letters match
+function fuzzyMatch(drivers, name){
+    function matchName(drivers){
+        return drivers.startsWith(name);
+    }
+    return drivers.filter(matchName);
+}
+
+function matchName(drivers, string){
+    function matchedName(driversName){
+        return driversName.name.toLowerCase() === string.toLowerCase();
+    }
+    return drivers.filter(matchedName);
+}
